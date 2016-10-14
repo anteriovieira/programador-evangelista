ac5851c7fa8210814e13e65961c9d74239bb3748
Jonatas Rodrigues <jhonatas.fender@gmail.com>
2016-10-14 13:46:33 -0300

    Alguns outros dons do Espírito Santo
    Estes métodos baseados na palavra de Deus.

--- DonsDoEspiritoSanto.php
+++ DonsDoEspiritoSanto.php
@@ -7,13 +7,20 @@ trait DonsDoEspiritoSanto
 	}
 
 	public function falarComSabedoria($palavras){
-		
 		// remover palavras torpes antes de falar 
-		
 		echo $palavras;
 	}
 
 	public function profetizar(){
 		return Deus::mandamento();
 	}
+	
+	public function palavraDeCiencia() {}
+	public function discernimentoDoEspiritos() {}
+	public function domDaFe() {}
+	public function cura() {}
+	public function operacaoDeMilagres() {}
+	public function variedadeDeLinguas() {}
+	public function DonsEspirituaisEFrutoDoEspiritoSanto () {}
+	public function DonsEspirituaisETalentos() {}	
 }
