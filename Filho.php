d68c0a9594ae3d42dd5d9fef4964572689f4b650
Antério Vieira <anteriovieira@gmail.com>
2016-10-14 12:56:44 -0300

    Update Filho.php

--- Filho.php
+++ Filho.php
@@ -5,9 +5,9 @@ final class Filho extends Jesus implements EspiritoSanto()
 {
 	use DonsDoEspiritoSanto;
 	
-    public function orar($oracao){
-	    // Nesse momento, oramos a jesus que interceda
-	    // por nós junto ao pai em nossas orações
+    	public function orar($oracao){
+	    	// Nesse momento, oramos a jesus que interceda
+	    	// por nós junto ao pai em nossas orações
 		JesusCristo::intecede($this, $oracao)
 	}
 }
