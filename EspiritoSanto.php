1f3826669a701dc25671f7bf40b3c355a75d7615
Antério Vieira <anteriovieira@gmail.com>
2016-10-14 12:58:31 -0300

    Create EspiritoSanto.php

---
+++ EspiritoSanto.php
@@ -0,0 +1,12 @@
+<?php
+
+// Espirito santo.
+interface EspiritoSanto 
+{
+	public function amar(){}
+	public function serPaciente(){}
+	public function serBondoso(){}
+	public function serFiel(){}
+	public function serManso(){}
+	public function terDominioProprio(){}
+}
